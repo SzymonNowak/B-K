bb0e20a872786e33e2c951928dbd9e1729b47ccc
SzymonNowak <nnja5@o2.pl>
2019-08-12 11:42:34 +0200

    router added

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import Home from './views/Home';
+import Contact from './views/Contact';
+import About from './views/About'
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { routes } from './routes/routes';
 class Root extends Component {
 
   render(){
     return(
-      <div>
-                 elo
-      </div>
+     <BrowserRouter>
+      <Switch>
+        <Route exact path={routes.home} component={Home} />
+        <Route exact path={routes.contact} component={Contact} />
+        <Route exact path={routes.about} component={About} />
+      </Switch>
+     </BrowserRouter>
     )
   }
 }
